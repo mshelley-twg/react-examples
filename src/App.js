9a3f34b4c488e883d1260b9d47ff623bf07086a3
Matt Shelley <mshelley@theworkinggroup.ca>
2019-07-22 14:45:33 -0400

    Implement Button component with useState to increment value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const Button = () => {
+  const [value, setValue] = useState(0)
+
+  return (
+    <button onClick={() => setValue(currentValue => currentValue + 1)}>{value}</button>
+  )
+}
 
 function App () {
   return (
     <main>
-      App will go here
+      <Button />
     </main>
   )
 }
