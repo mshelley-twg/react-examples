338c1a8f1b14abc75fffc1405e92cfc069e0010f
Matt Shelley <mshelley@theworkinggroup.ca>
2019-07-22 14:47:23 -0400

    Create a counter to increment and decrement a value

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react'
 
-const Button = () => {
+const Counter = () => {
   const [value, setValue] = useState(0)
 
   return (
-    <button onClick={() => setValue(currentValue => currentValue + 1)}>{value}</button>
+    <>
+      {value}
+      <button onClick={() => setValue(currentValue => currentValue + 1)}>Increment</button>
+      <button onClick={() => setValue(currentValue => currentValue - 1)}>Decrement</button>
+    </>
   )
 }
 
 function App () {
   return (
     <main>
-      <Button />
+      <Counter />
     </main>
   )
 }
