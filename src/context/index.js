20a09c3b0a4a28bc4379574f57f2b12732189b8f
Matt Shelley <mshelley@theworkinggroup.ca>
2019-07-23 14:21:59 -0400

    Toggle themes with context API

--- src/context/index.js
+++ src/context/index.js
@@ -1,22 +1,32 @@
-import React from 'react'
-import { ThemeContext, theme } from './theme-context'
+import React, { useState } from 'react'
+import { ThemeContext, themes } from './theme-context'
 
-const ThemeButton = ({ children }) => (
+const ThemeButton = ({ children, onClick }) => (
   <ThemeContext.Consumer>
     {theme => (
-      <button style={theme.button}>{children}</button>
+      <button style={theme.button} onClick={onClick}>{children}</button>
     )}
   </ThemeContext.Consumer>
 )
 
 function ContextExample () {
+  const [theme, setTheme] = useState(themes.light)
+
+  const toggleTheme = () => {
+    setTheme(theme => theme === themes.light ? themes.dark : themes.light)
+  }
+
   return (
-    <section>
-      <h2>Context</h2>
-      <ThemeContext.Provider value={theme}>
-        <ThemeButton>Button</ThemeButton>
-      </ThemeContext.Provider>
-    </section>
+    <ThemeContext.Provider value={theme}>
+      <ThemeContext.Consumer>
+        {theme => (
+          <section style={theme.example}>
+            <h2 style={theme.header}>Context</h2>
+            <ThemeButton onClick={toggleTheme}>Toggle Theme</ThemeButton>
+          </section>
+        )}
+      </ThemeContext.Consumer>
+    </ThemeContext.Provider>
   )
 }
 
--- src/context/theme-context.js
+++ src/context/theme-context.js
@@ -1,10 +1,30 @@
 import { createContext } from 'react'
 
-export const theme = {
-  button: {
-    color: '#ffffff',
-    background: '#008800'
+export const themes = {
+  light: {
+    example: {
+      background: '#ffffff'
+    },
+    header: {
+      color: '#000000'
+    },
+    button: {
+      color: '#ffffff',
+      background: '#00aa55'
+    }
+  },
+  dark: {
+    example: {
+      background: '#000033'
+    },
+    header: {
+      color: '#ffffff'
+    },
+    button: {
+      color: '#000000',
+      background: '#008800'
+    }
   }
 }
 
-export const ThemeContext = createContext(theme)
+export const ThemeContext = createContext(themes.light)
